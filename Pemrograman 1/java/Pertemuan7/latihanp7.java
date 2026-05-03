fc383ff2ec0914b7e315d3fbb95f6d1df02b9061
fasyafebriansyah91-cmyk <fasyafebriansyah91@gmail.com>
2026-05-03 23:32:50 +0700

    Add files via upload

---
+++ Pemrograman 1/java/Pertemuan7/latihanp7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class latihanp7 {
+    public static void main(String[] args){
+
+    int nilai = 90;
+
+    if(nilai>= 75){
+        System.out.println("lanjut");
+    } else {
+        System.out.println("tidak lanjut");
+    }
+    }
+}
